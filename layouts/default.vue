9d1fd18a5a02425c9751f67e98702a70f53bb279
schneefux <schneefux+commit@schneefux.xyz>
2019-09-08 11:08:54 +0200

    Add subheading with about text

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="min-h-screen flex flex-col justify-center bg-blue-900">
-    <div class="text-center">
-      <h1 class="text-5xl uppercase font-black text-gray-200">
+    <div class="text-center text-gray-200">
+      <h1 class="text-5xl uppercase font-black">
         f5inator
       </h1>
+      <h2 class="text-lg font-medium">
+        Keep this tab open for a notification when a website changes!
+      </h2>
     </div>
 
     <div class="mt-10 px-8 md:px-16 text-gray-300">
