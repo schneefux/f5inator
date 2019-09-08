1b8507abfd513ad899242471476b89cb356dac14
schneefux <schneefux+commit@schneefux.xyz>
2019-09-08 12:33:51 +0200

    Add background inator

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen flex flex-col justify-center bg-blue-900">
-    <div class="text-center text-gray-200">
+    <div class="text-center text-gray-200 z-10">
       <h1 class="text-5xl uppercase font-black">
         f5inator
       </h1>
@@ -9,14 +9,23 @@
       </h2>
     </div>
 
-    <div class="mt-10 px-8 md:px-16 text-gray-300">
+    <div class="mt-10 px-8 md:px-16 text-gray-300 z-10">
       <nuxt />
     </div>
+
+    <img
+      class="absolute w-35vmin bottom-0 right-0 mb-4 mr-6 z-0 opacity-75"
+      src="~/assets/inator.svg"
+    />
   </div>
 </template>
 
 <style>
 .min-h-screen {
   min-height: 100vh;
+}
+
+.w-35vmin {
+  width: 35vmin;
 }
 </style>
