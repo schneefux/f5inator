d84df5b8f8b3cd49d2392fb318313d7e460a5c34
schneefux <schneefux+commit@schneefux.xyz>
2019-09-14 12:42:08 +0200

    Update icon and description

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Monitor a website and get a notification when it changes.' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
   },
   loading: { color: '#9f7aea' },
   css: [
